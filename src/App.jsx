be6319e8fd2a141772198f4fa990c13e95ba2939
Michael Fray <michaelfray1@gmail.com>
2026-06-04 15:03:24 +0200

    Initial portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Michael Portfolio</h1>
+        <p>Web Developer Student | React & .NET</p>
+      </header>
+
+      <main>
+        <section>
+          <h2>Portfolio</h2>
+          <p>Here are my projects</p>
+        </section>
+      </main>
+    </div>
+  );
+}
+export default App;
