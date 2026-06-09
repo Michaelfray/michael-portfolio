0b5e9474e069a956fae10aea8b352b0eed251603
Michael Fray <michaelfray1@gmail.com>
2026-06-09 11:43:56 +0200

    Clean up Vite starter files and add portfolio navbar structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import "./App.css";
+
 
 function App() {
-  return (
-    <div>
-      <header>
-        <h1>Michael Portfolio</h1>
-        <p>Web Developer Student | React & .NET</p>
-      </header>
+  return <div className="min-h-screen" overflow-x-hidden">
+  <Navbar />
+  <main>
 
-      <main>
-        <section>
-          <h2>Portfolio</h2>
-          <p>Here are my projects</p>
-        </section>
-      </main>
-    </div>
-  );
+    
+  </main>
+
+  
 }
 export default App;
