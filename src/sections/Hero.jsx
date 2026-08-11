c09831d9d4db7181525cbb2a7c6cc760686891f7
Michael Fray <michaelfray1@gmail.com>
2026-08-11 15:49:31 +0200

    Add hero background and decorative dots

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,32 @@
+import heroBg from "../assets/img/hero-bg.jpg";
+
 export const Hero = () => {
-  return <section className="relative min-h-screen flex items-center overflow-hidden"></section>;
+  return (
+    <section className="relative min-h-screen flex items-center overflow-hidden">
+      {/* Bg */}
+      <div className="absolute inset-0">
+        <img
+          src={heroBg}
+          alt="Hero img"
+          className="w-full h-full object-cover opacity-40"
+        />
+        <div className="absolute inset-0 bg-gradient-to-b from-background/20 via-background/80 to-background via-background to-backgorund" />
+      </div>
+
+      {/* green dots */}
+      <div className="absolute inset-0 overflow-hidden pointer-events-none">
+        {[...Array(30)].map((_, i) => (
+          <div 
+          className="absolute w-1.5 h-1.5 rounded-full opacity-60"
+          style={{
+            backgroundColor: "#20B2A6",
+            left: `${Math.random() * 100}%`,
+            top: `${Math.random() * 100}%`,
+
+          }}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
